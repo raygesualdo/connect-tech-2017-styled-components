59ff2a78e3c505fe70b5e87119d2010948a8b54a
Ray Gesualdo <ray@rjgesualdo.com>
2017-09-22 10:30:41 -0400

    updated server config for public directory

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(
 
 app.use(require('webpack-hot-middleware')(compiler))
 
-app.use(express.static('public'))
+app.use('/public', express.static('public'))
 
 app.get('*', (req, res) => res.sendFile(join(__dirname, 'index.html')))
 
